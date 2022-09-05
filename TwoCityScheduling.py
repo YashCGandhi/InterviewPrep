923063b64e1125f2cb7db2e8679708992bae7da9
Rev0lver00 <35528420+Rev0lver00@users.noreply.github.com>
2022-09-05 14:11:10 -0700

    Create TwoCityScheduling.py

---
+++ TwoCityScheduling.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
+        diffs = []
+        res = 0
+        
+        costs.sort(key = lambda x : x[1] - x[0] )
+        
+        
+        for i in range(len(costs)):
+            if i < len(costs)//2:
+                res += costs[i][1]
+            else:
+                res += costs[i][0]
+                
+        return res
