a963cea3394a9724433977c7dd7ce1f82c32f532
Rev0lver00 <35528420+Rev0lver00@users.noreply.github.com>
2022-09-27 13:43:06 -0700

    Update 199BinaryTreeRightSideView.py

--- 199BinaryTreeRightSideView.py
+++ 199BinaryTreeRightSideView.py
@@ -1,23 +1,26 @@
-s Solution:
-    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        if not root:
-            return None
+def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
         output = []
+        if not root:
+            return []
         
-        q = [root, ]
+        queue = [root, ]
         
-        while q:
-            size = len(q)
+        while queue:
+            
+            # size is used to make sure that only the elements of the current level are being used
+            size = len(queue)
             
             while size > 0:
+                node = queue.pop(0)
                 
-                node = q.pop(0)
+                # add the left and right of node into the queue for next iteration
                 if node.left:
-                    q.append(node.left)
+                    queue.append(node.left)
                 if node.right:
-                    q.append(node.right)
-                size-=1
-                
+                    queue.append(node.right)
+    
+                size -= 1
+            # when the size becomes 0 the element will be the right most element in that level so append it to the output
             output.append(node.val)
         return output
-            
+        
