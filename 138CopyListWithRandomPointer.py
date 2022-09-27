ff71fa52c2ab3eb11f506960a25c11aedb15d67c
Rev0lver00 <35528420+Rev0lver00@users.noreply.github.com>
2022-09-26 18:46:57 -0700

    Update 138CopyListWithRandomPointer.py

--- 138CopyListWithRandomPointer.py
+++ 138CopyListWithRandomPointer.py
@@ -1,18 +1,20 @@
 def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
-        
+        # Create a hashmap to store the copy of the original linkedlist
         copyOfOld = { None : None }
         
+        # iterate over the LL and add the nodes in the hashmap
         cur = head
         while cur:
             copy = Node(cur.val)
             copyOfOld[cur] = copy
             
             cur = cur.next
-            
+        
+        # using the the original LinkedList assign next and random to the copies in the hashmap
         cur = head
         while cur:
             copy = copyOfOld[cur]
-            print(copyOfOld[cur.next], copyOfOld[cur.random])
+            #print(copyOfOld[cur.next], copyOfOld[cur.random])
             copy.next = copyOfOld[cur.next]
             copy.random = copyOfOld[cur.random]
             cur = cur.next
