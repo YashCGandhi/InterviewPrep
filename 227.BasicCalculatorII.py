10a6eaf8068aa46670c6283a7d262c1229b69812
Yash Chinmay Gandhi <35528420+YashCGandhi@users.noreply.github.com>
2022-12-30 15:59:37 -0800

    Update 227.BasicCalculatorII.py

--- 227.BasicCalculatorII.py
+++ 227.BasicCalculatorII.py
@@ -38,3 +38,39 @@ class Solution:
             i += 1
 
         return res
+    
+    
+# Using Stack
+"""
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = []
+        cur = 0
+        cur_operation = '+'
+        res = 0
+        i = 0
+        while i < len(s):
+            cur_char = s[i]
+
+            if cur_char.isdigit():
+                while i < len(s) and s[i].isdigit():
+                    cur = cur * 10 + int(s[i])
+                    i += 1
+                i -= 1
+
+                if cur_operation == '+':
+                     stack.append(cur)
+                elif cur_operation == '-':
+                    stack.append(-1 * cur)
+                elif cur_operation == '*':
+                    stack.append(stack.pop() * cur)
+                else:
+                    stack.append(int(stack.pop() / cur))
+                cur = 0
+            elif cur_char != ' ':
+                cur_operation = cur_char
+            i += 1
+        while stack:
+            res += stack.pop()
+        return res
+"""
