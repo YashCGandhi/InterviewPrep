5a03845ae474a97547c619e78b2b4521fe13bb8a
Yash Chinmay Gandhi <35528420+YashCGandhi@users.noreply.github.com>
2023-01-13 09:48:10 -0800

    Update 277.FindtheCelebrity.py

--- 277.FindtheCelebrity.py
+++ 277.FindtheCelebrity.py
@@ -17,3 +17,15 @@ class Solution:
             if cnt == n - 1:
                 return i
         return -1  
+ 
+        # T : O(n)
+        # As there is only one celebrity in a party, keep updating the candidate in the following manner, it will lead to the celeb if they exisit .
+        candidate = 0
+        for i in range(1,n):
+            if knows(candidate, i):
+                candidate = i
+        
+        for i in range(n):
+            if i != candidate and (knows(candidate,i) or not knows(i, candidate)):
+                return -1
+        return candidate
