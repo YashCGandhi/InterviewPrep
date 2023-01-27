8462f697bfa9b5c42cc53df10363839a55161453
Yash Chinmay Gandhi <35528420+YashCGandhi@users.noreply.github.com>
2023-01-26 17:48:47 -0800

    Update 362.DesignHitCounter.py

--- 362.DesignHitCounter.py
+++ 362.DesignHitCounter.py
@@ -1,15 +1,13 @@
 class HitCounter:
 
     def __init__(self):
-        self.ts = []
+        self.ts = deque([])
 
     def hit(self, timestamp: int) -> None:
         self.ts.append(timestamp)
 
     def getHits(self, timestamp: int) -> int:
-        hits = 0
-        for i in range(len(self.ts) - 1,-1, -1):
-            if -300 < self.ts[i] - timestamp <= 0:
-                hits += 1
-        return hits
+        while len(self.ts) > 0 and self.ts[0] + 300 <= timestamp:
+            self.ts.popleft()
+        return len(self.ts)
         
