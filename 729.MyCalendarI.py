06c6ca3e17b128d2205a90cff352e1b4369bc154
Yash Chinmay Gandhi <35528420+YashCGandhi@users.noreply.github.com>
2023-01-27 13:16:50 -0800

    Update 729.MyCalendarI.py

--- 729.MyCalendarI.py
+++ 729.MyCalendarI.py
@@ -1,3 +1,4 @@
+# T : O(N^2)
 class MyCalendar:
 
     def __init__(self):
@@ -9,6 +10,39 @@ class MyCalendar:
                 return False
         self.calender.append((start,end))
         return True
+
+
+# Binary Search - O(LogN) insertions
+
+class MyCalendar:
+
+    def __init__(self):
+        self.calender = []
+
+    def book(self, start: int, end: int) -> bool:
+        
+        l, r = 0, len(self.calender)
+
+        if r == 0:
+            self.calender.append((start, end))
+            return True
+        while l < r:
+            mid = (l + r) // 2
+            if self.calender[mid][1] <= start:
+                l = mid + 1
+            else:
+                r = mid
+
+        if l == len(self.calender):
+            self.calender.append((start,end))
+            return True
+        if self.calender[l][0] >= end:
+            self.calender.insert(l,(start, end))
+            return True
+
+        return False
+        
+
 
 
 # Your MyCalendar object will be instantiated and called as such:
