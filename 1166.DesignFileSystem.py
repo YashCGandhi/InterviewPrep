f9d6f5c7f2ec7a58cffbef6f8194585d68a3e258
Yash Chinmay Gandhi <35528420+YashCGandhi@users.noreply.github.com>
2023-01-27 14:35:51 -0800

    Update 1166.DesignFileSystem.py

--- 1166.DesignFileSystem.py
+++ 1166.DesignFileSystem.py
@@ -1,3 +1,4 @@
+# HashMap
 class FileSystem:
 
     def __init__(self):
@@ -21,8 +22,44 @@ class FileSystem:
     def get(self, path: str) -> int:
         return self.paths.get(path, -1)
 
+# HashMap + Trie
+class TrieNode:
+    def __init__(self, name):
+        self.map = defaultdict(TrieNode)
+        self.name = name
+        self.value = -1
 
-# Your FileSystem object will be instantiated and called as such:
-# obj = FileSystem()
-# param_1 = obj.createPath(path,value)
-# param_2 = obj.get(path)
+class FileSystem:
+
+    def __init__(self):
+        self.root = TrieNode("")        
+
+    def createPath(self, path: str, value: int) -> bool:
+        comp = path.split("/")
+
+        cur = self.root
+        for i in range(1, len(comp)):
+            name = comp[i]
+            if name not in cur.map:
+                if i == len(comp) - 1:
+                    cur.map[name] = TrieNode(name)
+                else:
+                    return False
+            cur = cur.map[name]
+        if cur.value != -1:
+            return False
+            
+        cur.value = value
+        return True
+
+    def get(self, path: str) -> int:
+        cur = self.root
+        comp = path.split("/")
+
+        for i in range(1, len(comp)):
+            name = comp[i]
+            if name not in cur.map:
+                return -1
+            cur = cur.map[name]
+        return cur.value
+
