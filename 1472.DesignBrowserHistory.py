43d08f57ed5fc289d59acb60dcdc2d3f00a8b888
Yash Chinmay Gandhi <35528420+YashCGandhi@users.noreply.github.com>
2023-01-28 21:18:15 -0800

    Update 1472.DesignBrowserHistory.py

--- 1472.DesignBrowserHistory.py
+++ 1472.DesignBrowserHistory.py
@@ -1,3 +1,6 @@
+
+# Solution Using Arrays
+
 class BrowserHistory:
 
     def __init__(self, homepage: str):
@@ -24,8 +27,34 @@ class BrowserHistory:
         return self.browser_history[self.cur_idx]
 
 
-# Your BrowserHistory object will be instantiated and called as such:
-# obj = BrowserHistory(homepage)
-# obj.visit(url)
-# param_2 = obj.back(steps)
-# param_3 = obj.forward(steps)
+# Solution Using Doubly LinkedList
+
+class ListNode:
+    def __init__(self,val):
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class BrowserHistory:
+
+    def __init__(self, homepage: str):
+        self.root = ListNode(homepage)
+
+    def visit(self, url: str) -> None:
+        node = ListNode(url)
+        node.prev = self.root
+        self.root.next = node
+        self.root = self.root.next
+
+
+    def back(self, steps: int) -> str:
+        while (steps and self.root.prev):
+            self.root = self.root.prev
+            steps -= 1
+        return self.root.val
+
+    def forward(self, steps: int) -> str:
+        while (steps and self.root.next):
+            self.root = self.root.next
+            steps -= 1
+        return self.root.val
