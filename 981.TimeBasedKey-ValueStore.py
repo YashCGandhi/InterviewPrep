1a9f28e01425699ecfe2c554dd68ce3d0f54b522
Yash Chinmay Gandhi <35528420+YashCGandhi@users.noreply.github.com>
2023-02-23 14:41:55 -0800

    Update 981.TimeBasedKey-ValueStore.py

--- 981.TimeBasedKey-ValueStore.py
+++ 981.TimeBasedKey-ValueStore.py
@@ -24,3 +24,30 @@ class TimeMap:
         
         return "" 
  
+
+
+class TimeMap:
+
+    def __init__(self):
+        self.records = {} # (value, timestamp)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.records.keys():
+            self.records[key] = []
+        self.records[key].append([value, timestamp])
+
+    def get(self, key: str, timestamp: int) -> str:
+        res = ""
+        if key not in self.records.keys():
+            return res
+
+        curRecord = self.records[key]
+        l, r = 0 , len(curRecord) - 1
+        while l <= r:
+            mid = (l + r) // 2
+            if curRecord[mid][1] <= timestamp:
+                res = curRecord[mid][0]
+                l = mid + 1
+            else:
+                r = mid - 1
+        return res
