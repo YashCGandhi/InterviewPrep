6fc7d62d5090b57d1fa24f7aa28f1776657369dd
Yash Chinmay Gandhi <35528420+YashCGandhi@users.noreply.github.com>
2023-03-03 21:01:28 -0800

    Update 25ReverseNodesin k-Group.py

--- 25ReverseNodesin k-Group.py
+++ 25ReverseNodesin k-Group.py
@@ -24,3 +24,35 @@ class Solution:
             prev = first
             switches -= 1
         return dummy.next
+    
+    
+# A little more understandable
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        
+        dummy = ListNode(0, head)
+        groupPrev = dummy
+        
+        while True:
+            kth = self.getKth(groupPrev, k)
+            if not kth:
+                break
+
+            groupNext = kth.next
+            prev, curr = kth.next, groupPrev.next
+            while curr != groupNext:
+                tmp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = tmp
+            
+            tmp = groupPrev.next
+            groupPrev.next = kth
+            groupPrev = tmp
+        return dummy.next
+    
+    def getKth(self, node, k):
+        while node and k > 0:
+            node = node.next
+            k -= 1
+        return node    
