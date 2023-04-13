3f9184ac99a95a7ad8ad941b1f215de75793cca9
Yash Chinmay Gandhi <35528420+YashCGandhi@users.noreply.github.com>
2023-04-13 09:27:59 -0700

    Update 53.MaximumSubarray.py

--- 53.MaximumSubarray.py
+++ 53.MaximumSubarray.py
@@ -1,5 +1,6 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        # Greedy mind being, " if the sum isn't positive its useless "
         curSum = nums[0]
         res = curSum
         for i in range(1, len(nums)):
