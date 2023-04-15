887e9d05dbf0b0d900a3a4eb28a0911103dad262
Yash Chinmay Gandhi <35528420+YashCGandhi@users.noreply.github.com>
2023-04-15 10:17:33 -0700

    Create 55.JumpGame.go

---
+++ 55.JumpGame.go
@@ -0,0 +1,13 @@
+func canJump(nums []int) bool {
+    goal := len(nums) - 1
+
+    for i := len(nums) - 2; i >= 0; i--{
+        if i + nums[i] >= goal{
+            goal = i
+        }
+    }
+    if goal == 0{
+        return true
+    }
+    return false
+}
