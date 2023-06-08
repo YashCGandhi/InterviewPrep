cdd6beb19f0314f7b322021bc09ff6c285726164
Yash Chinmay Gandhi <35528420+YashCGandhi@users.noreply.github.com>
2023-06-08 09:03:07 -0700

    Create 703.KthLargestElementinaStream.java

---
+++ 703.KthLargestElementinaStream.java
@@ -0,0 +1,33 @@
+class KthLargest {
+    private PriorityQueue<Integer> heap;
+    private int k;
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        heap = new PriorityQueue<>(k);
+        for (int n : nums){
+            if (heap.size() < k){
+                heap.offer(n);
+            }else if(n > heap.peek()){
+                heap.poll();
+                heap.offer(n);
+            }
+        }
+        
+    }
+    
+    public int add(int val) {
+        if (heap.size() < k){
+            heap.offer(val);
+        } else if(val > heap.peek()){
+            heap.poll();
+            heap.offer(val);
+        }
+        return heap.peek();
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
