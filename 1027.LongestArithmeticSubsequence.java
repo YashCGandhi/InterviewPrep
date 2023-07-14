9b81cc22bf7940abb10d9e8725cb10f011bd3e04
Yash Chinmay Gandhi <35528420+YashCGandhi@users.noreply.github.com>
2023-07-14 15:24:45 -0700

    Create 1027.LongestArithmeticSubsequence.java

---
+++ 1027.LongestArithmeticSubsequence.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestArithSeqLength(int[] nums) {
+        int n = nums.length;
+
+        if (n <= 2){
+            return n;
+        }
+
+        int ans = 2;
+        Map<Integer, Integer>[] dp = new HashMap[n];
+
+        for (int i = 0; i < n; i++){
+            dp[i] = new HashMap<>();
+            for (int j = 0; j < i; j++){
+                int diff = nums[i] - nums[j];
+                dp[i].put(diff, 1 + dp[j].getOrDefault(diff, 1));
+
+                ans = Math.max(ans, dp[i].get(diff));
+            }
+        }
+
+        return ans;
+    }
+}
