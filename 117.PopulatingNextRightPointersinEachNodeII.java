d9ffd6b5247bb9f84ef7b29a1ccc439333ff87bf
Yash Chinmay Gandhi <35528420+YashCGandhi@users.noreply.github.com>
2023-07-19 21:50:17 -0700

    Update 117.PopulatingNextRightPointersinEachNodeII.java

--- 117.PopulatingNextRightPointersinEachNodeII.java
+++ 117.PopulatingNextRightPointersinEachNodeII.java
@@ -1,3 +1,4 @@
+// Queue
 class Solution {
     public Node connect(Node root) {
         List<Node> q = new ArrayList<>();
@@ -26,5 +27,37 @@ class Solution {
         }
 
         return root; 
+    }
+}
+
+// Space Optimized solution.
+// We only move on to level N+1 when we are done establishing the next pointers for level N. So, since we have access to all the nodes on a particular level via the next pointers, we can use these next pointers to establish the connections for the next level or the level containing their children.
+class Solution {
+    public Node connect(Node root) {
+        if (root == null){
+            return root;
+        }
+        Node head = root;
+
+        while (head != null){
+            Node dummy = new Node(0);
+            Node tmp = dummy;
+
+            while (head != null){
+                if(head.left != null){
+                    tmp.next = head.left;
+                    tmp = tmp.next;
+                }
+
+                if(head.right != null){
+                    tmp.next = head.right;
+                    tmp = tmp.next;
+                }
+                head = head.next;
+            }
+            head = dummy.next;
+        }
+
+        return root;
     }
 }
